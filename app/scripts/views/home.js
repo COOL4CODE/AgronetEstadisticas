4482f5faeb592aa636a65567fec1e5cb3ce39dad
David Alméciga <wdavid@dagrinchi.com>
2014-12-30 09:29:15 -0500

    buscador en home

--- app/scripts/views/home.js
+++ app/scripts/views/home.js
@@ -13,6 +13,8 @@ define(function(require) {
 
 	var _ = require('underscore');
 	var homeTpl = require('text!tpl/home.html');
+	var SearchListView = require('views/searchList');
+	var Report = require('models/report');
 
 	var Marionette = require('backbone.marionette');
 
@@ -25,6 +27,25 @@ define(function(require) {
 		className: 'container-fluid',
 
 		template: _.template(homeTpl),
+
+		events: {
+			'click #searchBtn': 'searchEvent'
+		},
+
+		searchEvent: function(e) {
+			var reports = new Report.Collection();
+			var searchText = $('#searchText').val();
+
+			var searchListView = new SearchListView();
+			searchListView.data = searchText;
+			reports.searchText = searchText;
+			reports.fetch({
+				'success': function(reportsCollection) {
+					searchListView.collection = reportsCollection;
+					AgronetEstadisticas.searchRegion.show(searchListView);
+				}
+			});
+		},
 
 		regions: {
 			categoriesRegion: '#category',
